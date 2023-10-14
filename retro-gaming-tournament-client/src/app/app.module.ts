555852b26b94a5f0d17e4c898fcc72dc16ba3e00
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2023-10-14 02:03:37 +0200

    Created Select dropdown element to choose a game

--- retro-gaming-tournament-client/src/app/app.module.ts
+++ retro-gaming-tournament-client/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TournamentsComponent } from './tournaments/tournaments.component';
 import { PlayersComponent } from './players/players.component';
 import { GamesComponent } from './games/games.component';
 import { CreateTournamentComponent } from './create-tournament/create-tournament.component';
 import { GroupsComponent } from './groups/groups.component';
+import { CdkDropList, DragDropModule } from '@angular/cdk/drag-drop';
 
 
 @NgModule({
@@ -23,7 +25,11 @@ import { GroupsComponent } from './groups/groups.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CreateTournamentComponent
+    CreateTournamentComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    CdkDropList
   ],
   providers: [],
   bootstrap: [AppComponent]
--- retro-gaming-tournament-client/src/app/create-tournament/create-tournament.component.ts
+++ retro-gaming-tournament-client/src/app/create-tournament/create-tournament.component.ts
@@ -5,13 +5,17 @@ import {
   transferArrayItem,
   CdkDrag,
   CdkDropList,
+  DragDropModule,
 } from '@angular/cdk/drag-drop';
 import { NgFor } from '@angular/common';
 import { Player } from '../models/player.model';
 import { PlayerService } from '../services/player.service';
+import { Game } from '../models/game.model';
+import { GameService } from '../services/game.service';
 import { TournamentService } from '../services/tournament.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { forkJoin, map, mergeAll, mergeMap, toArray } from 'rxjs';
 
 @Component({
@@ -19,15 +23,22 @@ import { forkJoin, map, mergeAll, mergeMap, toArray } from 'rxjs';
   templateUrl: './create-tournament.component.html',
   styleUrls: ['./create-tournament.component.css'],
   standalone: true,
-  imports: [CdkDropList, NgFor, CdkDrag]
+  imports: [CdkDropList, NgFor, CdkDrag, ReactiveFormsModule, DragDropModule]
 })
 export class CreateTournamentComponent {
 
-  constructor(private playerService: PlayerService, private tournamentService: TournamentService, private router: Router, private http: HttpClient){}
+  //createTournamentForm!: FormGroup
+
+  constructor(private playerService: PlayerService, private tournamentService: TournamentService, private router: Router, private http: HttpClient, private gameService: GameService){}
 
   players : Player[] = []
   tournamentPlayers: Player[] = []
   arr : number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+  games : Game[] = []
+
+  createTournamentForm = new FormGroup({
+    game: new FormControl(),
+  })
  
 
   ngOnInit(){
@@ -35,6 +46,11 @@ export class CreateTournamentComponent {
       next: (data) =>    {
         this.players = data;
         this.tournamentPlayers  = []
+      }     
+    })
+    this.gameService.getGames().subscribe({
+      next: (data) =>    {
+        this.games = data;
       }     
     })
   }
@@ -52,7 +68,7 @@ export class CreateTournamentComponent {
     }
 
   }
-  onSubmitPlayers(event: Event){
+  onSubmit(event: Event){
     event.preventDefault();
     console.log(this.tournamentPlayers)
     this.tournamentService.getGroups(this.tournamentPlayers)
