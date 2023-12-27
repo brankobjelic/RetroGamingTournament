a8a5ea4f7b5e5cbf9e2234c2b28c6a95a8fb030b
33072204+brankobjelic@users.noreply.github.com <33072204+brankobjelic@users.noreply.github.com>
2023-12-27 19:07:42 +0100

    Migration with added stages

---
+++ RetroGamingTournamentApi/Migrations/20231227142241_newStages2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RetroGamingTournament.Migrations
+{
+    public partial class newStages2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Stages",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 8, "RankingPlay9-11" });
+
+            migrationBuilder.InsertData(
+                table: "Stages",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 9, "RankingPlay9-12" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Stages",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Stages",
+                keyColumn: "Id",
+                keyValue: 9);
+        }
+    }
+}
